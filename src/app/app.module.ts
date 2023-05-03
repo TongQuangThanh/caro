6db370610ddce2c21cbc59e714ffb038abe1dd1f
TongQuangThanh <tongquangthanh94@gmail.com>
2023-05-03 21:42:46 +0700

    to 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,9 @@ import { HomeComponent, ModalComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, PlayComponent, ModalComponent],
-  entryComponents: [],
-  imports: [FormsModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), SharedModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomeComponent, AboutComponent, PlayComponent, ModalComponent],
+    imports: [FormsModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), SharedModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
